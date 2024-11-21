06620ba7aecd72a71426b14c0ad7a174a0eeebb9
z2024z <unity2024z@outlook.com>
2024-11-22 00:32:24 +0800

    alks

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-console.log('hello world for node.js 2024');
+console.log('[hello world for node.js 2024 ]'+new Date(Date.now()));
 // console.log();
 // 2024-11-21 08:58:05
 // 2024-11-21 09:02:42
 // 2024-11-21 09:18:41
 // 2024-11-21 09:18:45 rain , not meeting-morning, start working.
 //lottery.schedule.moneyManage
+// 2024-11-22 00:03:17 record currentTime start.
+// How to ensure the life is continue.
+let huaBei20241122 = 2482.49 // repaymentDay 12-20
+let jieBei20241122 = 349.5 // repaymentDay 12-22
+let bankCard20241122 = 8069.98
+let weixinChangePocket = 660.42
+let eLeMe = 756.48 //12-10
+
+//I need  how much salary . to keep health env.
+//20 per month. salary
+//4 per month. subsidy
+
+// store data in sqlite3.
+console.log('[ write today status in sqlite3. ]'+new Date(Date.now()));
